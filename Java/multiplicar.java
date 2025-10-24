3a665fb8c416299d5323c19f50d8a0a30e32d5e5
pablo <pnunfe@gmail.com>
2025-10-24 17:08:48 +0200

    multiplicador en java

---
+++ Java/multiplicar.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class multiplicar {
+	public static void main(String[]args) {
+		Scanner sc=new Scanner(System.in);
+		int numero1;
+		int contador;
+		
+		System.out.print("Ingrese un número entero a multiplicar: ");
+		numero1=sc.nextInt();
+		
+		contador=0;
+		while(contador<=10) {
+			System.out.println(numero1+" x "+contador+" = "+(numero1*contador));
+			contador++;
+	}
+		sc.close();
+	}
+}
