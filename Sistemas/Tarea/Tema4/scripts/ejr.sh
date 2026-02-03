525384c1f8814f3289ee0e191dfede0668c2d614
Pablo Nuñez <pnunfe@gmail.com>
2026-02-03 17:17:45 +0100

    Add blank line at the beginning of holamundo.sh for improved readability

---
+++ Sistemas/Tarea/Tema4/scripts/ejr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Ejercicio 1
+echo $((10+5))
+echo $((10-5))
+echo $((10*5))
+echo $((10/5))
+echo $((10%5))
+echo $((10**5))
+echo $((10^5))
+echo $((10&5))
+echo $((10|5))
+
+#guardar el resultado de la suma en una variable    
+suma=$((10+5))
+echo "La suma es: $suma"
