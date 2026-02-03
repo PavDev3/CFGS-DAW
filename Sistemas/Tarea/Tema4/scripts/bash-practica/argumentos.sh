e8ccc0669b2529f64b20a6d8a300c63fccbdb42c
Pablo Nuñez <pnunfe@gmail.com>
2026-02-03 17:44:14 +0100

    Refactor holamundo.sh to enhance readability by adding a blank line at the beginning.

---
+++ Sistemas/Tarea/Tema4/scripts/bash-practica/argumentos.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+
+# Mostrar los argumentos
+echo "El primer argumento es: $1"
+echo "El segundo argumento es: $2"
+echo "Todos los argumentos son: $@"
+echo "El nombre del script es: $0"
---
+++ Sistemas/Tarea/Tema4/scripts/bash-practica/mi_script.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+#ejercicio 1 hara un echo para imprimir hola mundo!
+#echo "Hola, mundo"
+
+#ejercicio 2 crear una variable llamado nombre y otra saludo
+#nombre="MiApp"
+#saludo="Hola,"
+#echo "$saludo $nombre"
+
+
+if [[ ]]
