daa17761fcf3f2f1b5f7b8061bf3e091e145bd7e
pablo <pnunfe@gmail.com>
2026-02-27 15:06:05 +0100

    simulacro tienda

---
+++ Java/Tema4/src/testExam/ProductoInvalidoException.java
@@ -0,0 +1,8 @@
+package testExam;
+
+// Exception personalizada
+public class ProductoInvalidoException extends Exception {
+	public ProductoInvalidoException(String mensaje) {
+	super(mensaje);
+	}
+}
