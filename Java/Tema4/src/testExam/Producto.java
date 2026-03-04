351a9903684daca74dc59aa30da0d71896b0adb7
pablo <pnunfe@gmail.com>
2026-03-04 16:10:15 +0100

    tienda

--- Java/Tema4/src/testExam/Producto.java
+++ Java/Tema4/src/testExam/Producto.java
@@ -6,6 +6,7 @@ public class Producto {
 	private double precio;
 	private int stock;
 
+	
 	// constructor
 	public Producto(String nombre, double precio, int stock) {
 		this.nombre = nombre;
@@ -28,16 +29,17 @@ public class Producto {
 
 	// setter
 	public void setNombre(String nombre) {
-	    try {
-	        if (nombre == null || nombre.trim().isEmpty()) {
-	            throw new ProductoInvalidoException("El nombre no puede estar en blanco");
-	        }
-	        // Aquí puedes agregar el código para asignar el valor a la variable correspondiente
-	        System.out.println("Nombre establecido: " + nombre);
-	    } catch (ProductoInvalidoException e) {
-	        System.out.println("Error al establecer el nombre: " + e.getMessage());
+		try {
+			if (nombre == null || nombre.trim().isEmpty()) {
+				throw new ProductoInvalidoException("El nombre no puede estar en blanco");
+			}
+			// Aquí puedes agregar el código para asignar el valor a la variable
+			// correspondiente
+			System.out.println("Nombre establecido: " + nombre);
+		} catch (ProductoInvalidoException e) {
+			System.out.println("Error al establecer el nombre: " + e.getMessage());
 
-	    }
+		}
 	}
 
 	public void setPrecio(double precio) {
@@ -51,18 +53,20 @@ public class Producto {
 			System.out.println(e.getMessage());
 		}
 	}
+	
+	
 
 	public void setStock(int stock) {
-	    try {
-	        if (stock < 0) {
-	            throw new IllegalArgumentException("El stock no puede ser negativo.");
-	        }
-	        this.stock = stock;
-	        System.out.println("Stock establecido: " + stock);
-	    } catch (IllegalArgumentException e) {
-	        System.err.println("Error al establecer el stock: " + e.getMessage());
-	        
-	    }
+		try {
+			if (stock < 0) {
+				throw new IllegalArgumentException("El stock no puede ser negativo.");
+			}
+			this.stock = stock;
+			System.out.println("Stock establecido: " + stock);
+		} catch (IllegalArgumentException e) {
+			System.err.println("Error al establecer el stock: " + e.getMessage());
+
+		}
 	}
 
 }
--- Java/Tema4/src/testExam/ProductoInvalidoException.java
+++ Java/Tema4/src/testExam/ProductoInvalidoException.java
@@ -3,6 +3,6 @@ package testExam;
 // Exception personalizada
 public class ProductoInvalidoException extends Exception {
 	public ProductoInvalidoException(String mensaje) {
-	super(mensaje);
+		super(mensaje);
 	}
 }
