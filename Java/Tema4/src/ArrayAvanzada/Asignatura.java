b4b1227a5fb1b92a2cb2e2656cd61f52c9bd2dbb
pablo <pnunfe@gmail.com>
2026-03-10 21:24:56 +0100

    ejercicio ArrayAvanzado

---
+++ Java/Tema4/src/ArrayAvanzada/Asignatura.java
@@ -0,0 +1,22 @@
+package ArrayAvanzada;
+
+public class Asignatura {
+	// Atributos
+	private String nombre;
+	
+	// Constructor
+	public Asignatura(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	// Getters y Setters
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
